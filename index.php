8388aae43a7da0024baed89128851722c2fd7596
savin-dev <74615072+savin-dev@users.noreply.github.com>
2021-06-05 02:15:52 +0530

    create a calculator

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="">
+        <input type="text" name="num1" placeholder="Number 1">
+        <input type="text" name="num2" placeholder="Number 2">
+        <select name="operator" id="">
+            <option value="None">None</option>
+            <option value="Add">Add</option>
+            <option value="Subtract">Subtract</option>
+            <option value="Multiply">Multiply</option>
+            <option value="Divide">Divide</option>
+        </select>
+        <br />
+        <button type="submit" name="submit" value="submit">Claculator</button>
+    </form>
+    <p>The answer is:</p>
+
+    <?php
+    if (isset($_GET['submit'])) {
+        $result1 = $_GET['num1'];
+        $result2 = $_GET['num2'];
+        $operator = $_GET['operator'];
+        switch ($operator) {
+            case "None":
+                echo "You need to select a method.";
+                break;
+            case "Add":
+                echo $result1 + $result2;
+                break;
+            case "Subtract":
+                echo $result1 - $result2;
+                break;
+            case "Multiply":
+                echo $result1 * $result2;
+                break;
+            case "Divide":
+                echo $result1 / $result2;
+                break;
+        }
+    }
+    ?>
+
+</body>
+
+</html>
